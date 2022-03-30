d04ebb982f9dd56293bb5323df38cad5a050cfd1
User <User@LAPTOP-6K1H1QK0>
2022-03-30 15:31:28 +0530

    first commit

---
+++ day2/src/ex1/factorial.java
@@ -0,0 +1,24 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ener the number: ");
+		int num = sc.nextInt();
+		int i=1,fact=1;
+		while(i<=num)
+		{
+			fact=fact*i;
+			i++;
+		}
+		System.out.println("factorial of the number: "+fact);
+		
+		
+	 
+
+	}
+
+}
