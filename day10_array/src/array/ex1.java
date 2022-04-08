e1fcd735d0f73406cb24fcaf1d89a75254eb3d4a
User <User@LAPTOP-6K1H1QK0>
2022-04-08 18:51:58 +0530

    first commit

---
+++ day10_array/src/array/ex1.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		int arr[]= {5,10,15,20};
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		
+	}
+
+}
